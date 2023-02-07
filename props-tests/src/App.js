8a02dde7a55d1aef801e00ecd58ae1948ce14da1
Albert <dony.nyc@gmail.com>
2023-02-07 20:03:50 +0400

    state tests

--- props-tests/src/App.js
+++ props-tests/src/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import './App.css';
 
 // как работает под копотом?
@@ -11,16 +12,46 @@ import './App.css';
 // }
 
 // То же самое, но более правильно:
-function WhoAmI({ name, surname, link }) {
-  return (
-    <div>
-      <h1>My name is {name}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
-}
+// function WhoAmI({ name, surname, link }) {
+//   return (
+//     <div>
+//       <h1>My name is {name}, surname - {surname}</h1>
+//       <a href={link}>My profile</a>
+//     </div>
+//   )
+// }
 // В качестве пропсов можно использовать любой тип информации, 
 // включая объекты и готовые функции
+
+// Переделаем в классы:
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++'
+    }
+  }
+
+  nextYear = () => {
+    console.log('+++');
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+        <br />
+        <button onClick={this.nextYear}>{this.state.text}</button>
+      </div >
+    )
+  }
+}
 
 function App() {
   return (
